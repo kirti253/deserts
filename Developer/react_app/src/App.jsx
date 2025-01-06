e90267612149e164a96c1d95bceff1c7dd574301
kirti <kirti25032007@gmail.com>
2025-01-06 12:28:47 +0530

    basic structure

--- Developer/react_app/src/App.jsx
+++ Developer/react_app/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>helo thrre</h1>
+    </div>
+  );
 }
 
-export default App
+const style = {
+  width: 200,
+  backgroundColor: "white",
+  borderRadius: 10,
+  borderColor: "grey",
+  borderWidth: 1,
+};
+function postComponent() {
+  return (
+    <div style={style}>
+      <img
+        src={""}
+        style={{
+          width: 20,
+          height: 20,
+          borderRadius: 20,
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
